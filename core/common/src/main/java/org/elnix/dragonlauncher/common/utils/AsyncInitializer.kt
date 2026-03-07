be046d484e99150b44aa14e2da7fc5b61b226f64
Elnix90 <elnix91@gmail.com>
2026-03-07 20:36:59 +0100

    fix: removed unused val

--- core/common/src/main/java/org/elnix/dragonlauncher/common/utils/AsyncInitializer.kt
+++ core/common/src/main/java/org/elnix/dragonlauncher/common/utils/AsyncInitializer.kt
@@ -1,12 +1,10 @@
 package org.elnix.dragonlauncher.common.utils
 
 import android.content.Context
-import androidx.core.content.ContextCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.elnix.dragonlauncher.common.logging.DragonLogManager
 import timber.log.Timber
@@ -17,12 +15,11 @@ import timber.log.Timber
 object AsyncInitializer {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private val _isInitialized = MutableStateFlow(false)
-    val isInitialized: StateFlow<Boolean> = _isInitialized
 
     fun init(context: Context) {
         scope.launch {
             val startTime = System.currentTimeMillis()
-            
+
             // 1. Logging (Critical but fast)
             DragonLogManager.init(context)
             Timber.d("AsyncInitializer: DragonLogManager initialized")
