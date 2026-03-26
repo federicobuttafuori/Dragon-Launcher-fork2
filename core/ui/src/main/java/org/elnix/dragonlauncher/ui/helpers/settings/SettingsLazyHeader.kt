045eedf34488659ca6c03d3485f1b4dbf550c33a
Elnix90 <elnix91@proton.me>
2026-03-26 21:42:55 +0100

    fix: fixed bottom content drawing

--- core/ui/src/main/java/org/elnix/dragonlauncher/ui/helpers/settings/SettingsLazyHeader.kt
+++ core/ui/src/main/java/org/elnix/dragonlauncher/ui/helpers/settings/SettingsLazyHeader.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
@@ -78,25 +79,25 @@ fun SettingsLazyHeader(
             .windowInsetsPadding(WindowInsets.safeDrawing.exclude(WindowInsets.ime))
     ) {
 
-        Column (
+        Column(
             modifier = Modifier
                 .padding(horizontal = 16.dp)
                 .imePadding(),
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-           Column (
+            Column(
                 modifier = Modifier
                     .fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(5.dp)
             ) {
-                   SettingsTitle(
-                        title = title,
-                        otherIcons = otherIcons,
-                        helpIcon = { showHelpDialog = true },
-                        resetIcon = if (onReset != null) {
-                            { showResetDialog = true }
-                        } else null,
-                    ) { onBack() }
+                SettingsTitle(
+                    title = title,
+                    otherIcons = otherIcons,
+                    helpIcon = { showHelpDialog = true },
+                    resetIcon = if (onReset != null) {
+                        { showResetDialog = true }
+                    } else null,
+                ) { onBack() }
 
 
                 if (titleContent != null) titleContent()
@@ -136,7 +137,10 @@ fun SettingsLazyHeader(
                 }
             }
 
-            if (bottomContent != null) bottomContent()
+            if (bottomContent != null) {
+                Spacer(Modifier.weight(1f))
+                bottomContent()
+            }
         }
     }
 
