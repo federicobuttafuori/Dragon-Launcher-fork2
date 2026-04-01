727ca146486c3f4080f3963534aeaaaf04be877d
Elnix90 <elnix91@proton.me>
2026-04-01 12:06:25 +0200

    fix: updated UI on action row, using material shapes and haptic feedback

--- core/ui/src/main/kotlin/org/elnix/dragonlauncher/ui/components/generic/ActionRow.kt
+++ core/ui/src/main/kotlin/org/elnix/dragonlauncher/ui/components/generic/ActionRow.kt
@@ -1,28 +1,32 @@
 package org.elnix.dragonlauncher.ui.components.generic
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonGroup
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.elnix.dragonlauncher.common.utils.semiTransparentIfDisabled
-import org.elnix.dragonlauncher.ui.modifiers.shapedClickable
+import org.elnix.dragonlauncher.ui.UiConstants
+import org.elnix.dragonlauncher.ui.colors.AppObjectsColors
+import org.elnix.dragonlauncher.ui.helpers.withHaptic
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun <T> ActionRow(
     actions: List<T>,
@@ -33,15 +37,16 @@ fun <T> ActionRow(
     actionIcon: @Composable ((T) -> ImageVector)? = null,
     onClick: (T) -> Unit
 ) {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Max),
-        horizontalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        actions.forEach { mode ->
-            val isSelected = mode == selectedView
+    val interactionSources = remember { List(actions.size) { MutableInteractionSource() } }
 
+    @Suppress("DEPRECATION")
+    ButtonGroup(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max)
+    ) {
+        actions.forEachIndexed { index, mode ->
+            val isSelected = mode == selectedView
 
             val backgroundColor = (
                     if (isSelected) MaterialTheme.colorScheme.primary
@@ -53,19 +58,14 @@ fun <T> ActionRow(
                     else MaterialTheme.colorScheme.onSurface
                     ).semiTransparentIfDisabled(enabled)
 
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
+            Button(
+                onClick = withHaptic { onClick(mode) },
+                shapes = UiConstants.dragonShapes(),
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxHeight()
-                    .shapedClickable(
-                        enabled = enabled,
-                        isSelected = isSelected,
-                        onClick = { onClick(mode) }
-                    )
-                    .background(backgroundColor)
-                    .padding(5.dp)
+                    .animateWidth(interactionSources[index]),
+                interactionSource = interactionSources[index],
+                colors = AppObjectsColors.buttonColors(backgroundColor)
             ) {
                 actionIcon?.let {
                     Icon(
@@ -83,6 +83,7 @@ fun <T> ActionRow(
                             .padding(12.dp),
                         color = textColor,
                         style = MaterialTheme.typography.bodySmall,
+                        fontSize = 10.sp,
                         textAlign = TextAlign.Center
                     )
                 }
