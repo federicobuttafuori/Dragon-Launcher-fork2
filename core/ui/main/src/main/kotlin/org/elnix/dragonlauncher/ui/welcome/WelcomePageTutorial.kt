2ca2b68e57d6b23c42172a5a30bb74953495f5e8
Elnix90 <elnix91@proton.me>
2026-04-07 20:27:35 +0200

    fix: edited pictures in welcome screen and updated welcome page tutorial

--- core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/welcome/WelcomePageTutorial.kt
+++ core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/welcome/WelcomePageTutorial.kt
@@ -3,9 +3,7 @@ package org.elnix.dragonlauncher.ui.welcome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -37,72 +35,35 @@ fun WelcomePageTutorial() {
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(15.dp)
         ) {
+            TutorialEntry(R.drawable.long_click_3second,R.string.long_click_to_access_settings)
+            TutorialEntry(R.drawable.configure_your_apps,R.string.configure_your_apps)
+            TutorialEntry(R.drawable.swipe_to_open_app,R.string.swipe_to_open_app)
+        }
+    }
+}
 
-            Card(
-                colors = AppObjectsColors.cardColors(),
-            ){
-                Column(
-                    modifier = Modifier.padding(5.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(
-                        text = stringResource(R.string.long_click_to_access_settings),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.padding(5.dp)
-                        )
+@Composable
+private fun TutorialEntry(
+    painterResId: Int,
+    titleResId: Int
+) {
+    Card(colors = AppObjectsColors.cardColors()) {
+        Column(
+            modifier = Modifier.padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(titleResId),
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.padding(5.dp)
+            )
 
-                    Image(
-                        painterResource(R.drawable.long_click_3second),
-                        contentDescription = stringResource(R.string.long_click_to_access_settings),
-                    )
-                }
-            }
-
-            Spacer(Modifier.height(15.dp))
-
-            Card(
-                colors = AppObjectsColors.cardColors(),
-            ){
-                Column(
-                    modifier = Modifier.padding(5.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(
-                        text = stringResource(R.string.configure_your_apps),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.padding(5.dp)
-                    )
-
-                    Image(
-                        painterResource(R.drawable.configure_your_apps),
-                        contentDescription = stringResource(R.string.configure_your_apps),
-                    )
-                }
-            }
-
-            Spacer(Modifier.height(15.dp))
-
-            Card(
-                colors = AppObjectsColors.cardColors(),
-            ){
-                Column(
-                    modifier = Modifier.padding(5.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(
-                        text = stringResource(R.string.swipe_to_open_app),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.padding(5.dp)
-                    )
-
-                    Image(
-                        painterResource(R.drawable.swipe_to_open_app),
-                        contentDescription = stringResource(R.string.swipe_to_open_app),
-                    )
-                }
-            }
+            Image(
+                painterResource(painterResId),
+                contentDescription = stringResource(titleResId),
+            )
         }
     }
 }
